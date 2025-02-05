b5ddbae35987d71a93daca039aa2ca9ca8f2e247
margaux-works <margaux.espinasse@gmail.com>
2025-02-05 13:15:45 +0100

    hold shift to check multiple checkboxes

--- 30days-challenge/day-10/script.js
+++ 30days-challenge/day-10/script.js
@@ -1,8 +1,27 @@
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 console.log(checkboxes);
 
+let lastChecked;
+
 function handleCheck(e) {
-  console.log(e);
+  // check if user had the shift key down
+  // AND check that they are checking it (not unchecking)
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    // if so, loop over every single checkbox
+    checkboxes.forEach((checkbox) => {
+      console.log(checkbox);
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+        console.log('starting to check them inbetween!');
+      }
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+
+  lastChecked = this;
 }
 
 checkboxes.forEach((checkbox) =>
